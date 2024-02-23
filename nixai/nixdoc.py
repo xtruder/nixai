9b8a3ce09dace229a98ae4d3e00bc02df8806f58
Jaka Hudoklin <jaka@x-truder.net>
2024-02-23 08:04:20 +0000

    Attempts to use ragatouille

--- nixai/nixdoc.py
+++ nixai/nixdoc.py
@@ -1,43 +1,50 @@
-import argparse
 import uuid
 
 from typing import Any, List, Optional
+from argparse import ArgumentParser
 
+from langchain_core.callbacks import CallbackManagerForRetrieverRun
 from langchain_core.documents import Document
+from langchain_core.retrievers import BaseRetriever
+from langchain_core.stores import BaseStore
+from langchain_core.pydantic_v1 import Field
 
-from langchain.text_splitter import RecursiveCharacterTextSplitter, Language, MarkdownHeaderTextSplitter
-from langchain.retrievers import MultiVectorRetriever
+from langchain.text_splitter import TextSplitter
+from langchain.text_splitter import MarkdownHeaderTextSplitter, RecursiveCharacterTextSplitter
 
-from langchain_community.embeddings.fastembed import FastEmbedEmbeddings
-from langchain_community.vectorstores.chroma import Chroma
 from langchain_community.storage.sql import SQLDocStore
 
-from nixai.settings import chroma_dir, docstore_conn_str
+from ragatouille import RAGPretrainedModel
+
+from nixai.settings import docstore_conn_str
+
 
 collection_name = "nixdoc"
 
-embeddings = FastEmbedEmbeddings(
-    model_name="BAAI/bge-base-en-v1.5",
-    max_length=768, threads=16,
-    cache_dir="/tmp/fastembedding")
 
-# load from disk
-vectorstore = Chroma(
-    persist_directory=chroma_dir,
-    embedding_function=embeddings,
-    collection_name=collection_name)
+class ParentDocumentRetriever(BaseRetriever):
+    rag: RAGPretrainedModel
+    """Ragatouille rag to use"""
 
-store = SQLDocStore(docstore_conn_str, collection_name=f"chroma/{collection_name}") 
+    index_name: str
+    """Index name to use"""
 
-class ParentDocumentRetriever(MultiVectorRetriever):
-    child_splitter: Any
+    child_splitter: TextSplitter
     """The text splitter to use to create child documents."""
 
+    id_key: str = "id"
     """The key to use to track the parent id. This will be stored in the
     metadata of child documents."""
-    parent_splitter: Optional[MarkdownHeaderTextSplitter] = None
+
+    parent_splitter: Optional[Any] = None
     """The text splitter to use to create parent documents.
     If none, then the parent documents will be the raw documents passed in."""
+
+    docstore: BaseStore[str, Document]
+    """The storage interface for the parent documents"""
+
+    search_kwargs: dict = Field(default_factory=dict)
+    """Keyword arguments to pass to the search function."""
 
     def add_documents(
         self,
@@ -83,7 +90,7 @@ class ParentDocumentRetriever(MultiVectorRetriever):
                 )
             doc_ids = ids
 
-        docs = []
+        docs: List[Document] = []
         full_docs = []
         for i, doc in enumerate(documents):
             _id = doc_ids[i]
@@ -92,54 +99,74 @@ class ParentDocumentRetriever(MultiVectorRetriever):
                 _doc.metadata[self.id_key] = _id
             docs.extend(sub_docs)
             full_docs.append((_id, doc))
-        self.vectorstore.add_documents(docs)
+
+        # index sub documents to vectorstore
+        self.rag.index(
+            [d.page_content for d in docs],
+            document_metadatas=[d.metadata for d in docs],
+            index_name=self.index_name)
+
+        # add documents to document store
         if add_to_docstore:
             self.docstore.mset(full_docs)
 
+    def _get_relevant_documents(
+        self, query: str, *, run_manager: CallbackManagerForRetrieverRun
+    ) -> List[Document]:
+        """Get documents relevant to a query.
+        Args:
+            query: String to find relevant documents for
+            run_manager: The callbacks handler to use
+        Returns:
+            List of relevant documents
+        """
+        search_result = self.rag.search(query, **self.search_kwargs)
 
-# separators = RecursiveCharacterTextSplitter.get_separators_for_language(Language.HTML)
-parent_splitter = MarkdownHeaderTextSplitter(
+        sub_docs = [
+            Document(
+                page_content=doc["content"], metadata=doc.get("document_metadata", {})
+            )
+            for doc in search_result
+        ]
+
+        # We do this to maintain the order of the ids that are returned
+        ids = []
+        for d in sub_docs:
+            if self.id_key in d.metadata and d.metadata[self.id_key] not in ids:
+                ids.append(d.metadata[self.id_key])
+
+        docs = self.docstore.mget(ids)
+
+        return [d for d in docs if d is not None]
+
+store = SQLDocStore(docstore_conn_str, collection_name=f"chroma/{collection_name}") 
+
+md_splitter = MarkdownHeaderTextSplitter(
     headers_to_split_on=[("#", "Header 1"), ("##", "Header 2"), ("###", "Header 3"), ("####", "Header 4")],
     strip_headers=False)
-child_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=100)
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=180, chunk_overlap=10)
 
 retriever = ParentDocumentRetriever(
-    vectorstore=vectorstore,
     docstore=store,
-    child_splitter=child_splitter,
-    parent_splitter=parent_splitter,
-    search_type="similarity",
+    rag=RAGPretrainedModel.from_index(".ragatouille/colbert/indexes/"+collection_name),
+    parent_splitter=md_splitter,
+    child_splitter=text_splitter,
+    index_name=collection_name,
     search_kwargs={"k": 3},
 )
 
-# retriever = vectorstore.as_retriever(search_kwargs={"k": 4})
-
 def embed_documents(path, **_):
     from langchain_community.document_loaders import DirectoryLoader, UnstructuredMarkdownLoader
-    #from langchain.text_splitter import MarkdownHeaderTextSplitter, RecursiveCharacterTextSplitter
 
     # load all markdown files from directory
     loader = DirectoryLoader(path, glob="**/*.md", show_progress=True, loader_cls=UnstructuredMarkdownLoader, silent_errors=True)
-    # md_splitter = MarkdownHeaderTextSplitter(
-    #     headers_to_split_on=[("#", "Header 1"), ("##", "Header 2"), ("###", "Header 3"), ("####", "Header 4")])
-    # text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
 
     print(f"Loading documents")
 
     documents = loader.load()
 
-    # print(f"Splitting {len(documents)} documents")
-
-    # all_docs = []
-    # for doc in documents:
-    #     docs = md_splitter.split_text(doc.page_content)
-    #     docs = text_splitter.split_documents(docs)
-
-    #     all_docs.extend(docs)
-
     print(f"Embedding {len(documents)} documents")
 
-    #vectorstore.add_documents(all_docs)
     retriever.add_documents(documents)
 
 def query_documents(query, **_):
@@ -149,7 +176,7 @@ def query_documents(query, **_):
 
 if __name__ == "__main__":
     # Initialize the ArgParser with a description
-    parser = argparse.ArgumentParser(description='nixdoc management script')
+    parser = ArgumentParser(description='nixdoc management script')
 
     # Create subparsers for the two different commands
     subparsers = parser.add_subparsers(dest='command', help='Sub-command help')
